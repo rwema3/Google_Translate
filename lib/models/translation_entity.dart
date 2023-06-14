5a1ac5736fba755340d483b847ab597f53838fa1
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-06-14 18:47:53 +0200

    Translation

--- lib/models/translation_entity.dart
+++ lib/models/translation_entity.dart
@@ -19,3 +19,4 @@ class TranslationEntity with WithPreferencesSerializable {
   static TranslationEntity fromMap(Map<String, Object> data) {
     return TranslationEntity(
         source: data['source'],
+        translation: data['translation'],
