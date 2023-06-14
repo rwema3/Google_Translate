47cc7c9c61846659b5c45f7928c821dbe1289515
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-06-14 18:45:03 +0200

    Translation Entity

--- lib/models/translation_entity.dart
+++ lib/models/translation_entity.dart
@@ -2,3 +2,4 @@ import 'package:crypted_preferences/crypted_preferences.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_translate_s/models/languages/language.dart';
 
+class TranslationEntity with WithPreferencesSerializable {
